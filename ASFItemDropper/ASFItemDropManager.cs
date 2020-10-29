eb9f7e33a76682247eadfbf0832fee8623c00b81
Sniper677 <sniper677@arcor.de>
2020-10-29 23:01:38 +0100

    setting 'args.Length == null'

--- ASFItemDropper/ASFItemDropManager.cs
+++ ASFItemDropper/ASFItemDropManager.cs
@@ -50,7 +50,7 @@ namespace ASFItemDropManager
                             return await CheckItem(steamID, args[1], args[2], Utilities.GetArgsAsText(args, 3, ",")).ConfigureAwait(false);
                         case "IDROP" when args.Length > 2:
                             return await CheckItem(steamID, bot, args[1], Utilities.GetArgsAsText(args, 2, ",")).ConfigureAwait(false);
-                        case "IDROPDEFLIST" when args.Length >= 0:
+                        case "IDROPDEFLIST" when args.Length == null:
                             return await ItemDropDefList(steamID, bot).ConfigureAwait(false);
                         default:
                             return null;
